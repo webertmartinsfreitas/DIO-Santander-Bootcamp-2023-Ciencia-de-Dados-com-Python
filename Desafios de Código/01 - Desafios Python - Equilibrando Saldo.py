95750fd415d6f59ed8334ca6933383f822ac90ce
Webert Martins de Freitas <68933320+webertmartinsfreitas@users.noreply.github.com>
2023-08-23 15:22:19 -0300

    Delete 01 - Desafios Python - Equilibrando Saldo.py

--- Desafios de Código/01 - Desafios Python - Equilibrando Saldo.py
+++
@@ -1,7 +0,0 @@
-saldo_atual = float(input())
-valor_deposito = float(input())
-valor_retirada = float(input())
-
-saldo_atualizado = saldo_atual + valor_deposito - valor_retirada
-
-print(f"Saldo atualizado na conta: {saldo_atualizado:.1f}")
